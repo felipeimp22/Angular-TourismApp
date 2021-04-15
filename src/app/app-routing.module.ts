2f799d16ab014f3bf0403abe78bacef990c04583
felipeimp22 <felipeimperio.imperioo@gmail.com>
2021-04-15 16:20:40 -0300

    Feature/Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListaComponent } from './pagina/lista/lista.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { HomeComponent } from './pagina/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Contact', component: ContatoComponent },
+  { path: 'List', component: ListaComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
