29f91c783014a2d315a81f17f5ee7a0701c72f0c
felipeimp22 <felipeimperio.imperioo@gmail.com>
2021-04-15 15:46:19 -0300

    Feature/ApplyingComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { HomeComponent } from './pagina/home/home.component';
+import { MenuComponent } from './pagina/menu/menu.component';
+import { ListaComponent } from './pagina/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    HomeComponent,
+    MenuComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
